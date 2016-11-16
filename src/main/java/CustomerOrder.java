0b83a826f5155244a53cc822f243ac4e836c7094
Jake <jstewart7@hotmail.co.uk>
2016-11-16 14:46:07 +0000

    customer order updated

--- src/main/java/CustomerOrder.java
+++ src/main/java/CustomerOrder.java
@@ -1,24 +1,67 @@
+import javax.persistence.*;
+import javax.validation.constraints.*;
 
+@Entity
+@Table(name="CustomerOrder")
 public class CustomerOrder {
+	
+	
+	
+	@Id
+	@Column(name="idCustomerOrder", nullable=false, unique=true)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int idCustomerOrder;
+		
+	
+	@Column(name="Customerstatus", nullable=false, length=45)
+	@Size(max=225)
+	@NotNull
 	private String status;
-	private int cardDetails;
-	private int customerId;
+	
+	
+	
+	
+	@Column(name="cardDetails", nullable=false)
+	@NotNull
+	private CardDetails cardDetails;
+	
+	@Column(name="customerID", nullable=false)
+	@NotNull
+	private int customerID;
+	
+	
+	@Column(name="dateStampPlaced", nullable=false)
+	@NotNull
 	private String dateStampPlaced;
-	private int addressId;
+	
+	@Column(name="addressId", nullable=false)
+	private Address Address;
+	
+	
+	
+	
 	private int billingAddress;
 	private int cardNumber;
 	private String dateTimeRecieved; // the date the customer receives order
 
+//method	
+	
 	public CustomerOrder() {
 	}
 
-	public CustomerOrder(int idCustomerOrder, String status, int cardDetails, int customerId, String dateStampPlaced,
+//Constructors
+	public CustomerOrder(int idCustomerOrder, String status, int cardDetails, int customerID, String dateStampPlaced,
 			int addressId, int billingAddress, int cardNumber, String dateTimeRecieved) {
 	}
 
-	public CustomerOrder(int idCustomerOrder, String status, int cardDetails, int customerId, String dateStampPlaced,
+	public CustomerOrder(int idCustomerOrder, String status, int cardDetails, int customerID, String dateStampPlaced,
 			int addressId, int billingAddress, int cardNumber) {
 	}
 
+//DB to entities
+	
+	
+	
+	
+	
 }
