52250d0d1f54eadbd4360a30df8dd4a2857a5229
Sophie <sophiegreenfield31@msn.com>
2016-11-16 14:33:34 +0000

    card details and cust order line added

--- src/main/java/CustomerOrderLine.java
+++ src/main/java/CustomerOrderLine.java
@@ -1,8 +1,34 @@
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name="customerOrderLine")
 public class CustomerOrderLine {
-	
+
+	@Column(name = "Customer Order", nullable = false, length=255)
+	@Size(max=255)
+	@NotNull
 	private int customerOrder;
+	
+	@Column(name = "Product ID", nullable = false, length=255)
+	@Size(max=255)
+	@NotNull
 	private int productID;
+	
+	@Column(name = "Quantity", nullable = false, length=255)
+	@Size(max=255)
+	@NotNull
 	private int quantity;
+	
+	@Column(name = "Order Line Prices", nullable = false, length=255)
+	@Size(max=255)
+	@NotNull
 	private String orderLinePrice;
+
+	public void customerOrderLine() {
+	}
+
+	public CustomerOrderLine(int customerOrder, int productID, int quantity, String orderLinePrice) {
+	}
 }
