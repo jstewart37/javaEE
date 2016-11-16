913071f1a3ed1017f6ef72ab62f5cdf61e13322d
Sophie <sophiegreenfield31@msn.com>
2016-11-16 15:26:47 +0000

    card details and cust order line added

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -1,5 +1,6 @@
 import javax.annotation.Generated;
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,36 +11,44 @@ import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "addresses")
+@Embeddable
 public class Address {
-	
+
 	// variables
-	@Id @Column(name ="id_Address", nullable = false, unique = true) @GeneratedValue(strategy = GenerationType.IDENTITY)private long idAddress;
-	@Column(name ="address_Line_1", nullable = false, length = 100)@Size(max=100) @NotNull private String addressLine1;
-	@Column(name ="address_Line_2", length = 100) @Size(max=100) private String addressLine2;
-	@Column(name ="postcode", nullable = false, length = 10) @Size(max=10) @NotNull private String postCode;
-	@Column(name ="county", nullable = false, length = 100) @Size(max=100) @NotNull private String county;
-	@Column(name ="city", nullable = false, length = 100) @Size(max=100) @NotNull private String city;
-	
-	
+	@Id
+	@Column(name = "id_Address", nullable = false, unique = true)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idAddress;
+	@Column(name = "address_Line_1", nullable = false, length = 100)
+	@Size(max = 100)
+	@NotNull
+	private String addressLine1;
+	@Column(name = "address_Line_2", length = 100)
+	@Size(max = 100)
+	private String addressLine2;
+	@Column(name = "postcode", nullable = false, length = 10)
+	@Size(max = 10)
+	@NotNull
+	private String postCode;
+	@Column(name = "county", nullable = false, length = 100)
+	@Size(max = 100)
+	@NotNull
+	private String county;
+	@Column(name = "city", nullable = false, length = 100)
+	@Size(max = 100)
+	@NotNull
+	private String city;
+
 	// private customer_id FOREIGN KEYS
 	// private supplier_id FOREIGN KEYS
 
-	
 	// constructors
-	public Address(long idAddress,
-			String addressLine1,
-			String addressLine2,
-			String postCode,
-			String County,
-			String city)
-	{}
-	
-	public Address(long idAddress,
-			String addressLine1,
-			String postCode,
-			String County,
-			String city)
-	{}
+	public Address(long idAddress, String addressLine1, String addressLine2, String postCode, String County,
+			String city) {
+	}
+
+	public Address(long idAddress, String addressLine1, String postCode, String County, String city) {
+	}
 
 	// getters & setters
 	public long getIdAddress() {
@@ -89,8 +98,5 @@ public class Address {
 	public void setCity(String city) {
 		this.city = city;
 	}
-					
-	
 
-	
 }
--- src/main/java/CustomerOrderLine.java
+++ src/main/java/CustomerOrderLine.java
@@ -2,27 +2,32 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@IdClass(Product.class)
 @Entity
-@Table(name="customerOrderLine")
+@Table(name = "customerOrderLine")
 public class CustomerOrderLine {
 
-	@Column(name = "Customer Order", nullable = false, length=255)
-	@Size(max=255)
+	@Id
+	@ManyToOne
+	@Column(name = "Customer Order", nullable = false, length = 255)
+	@Size(max = 255)
 	@NotNull
 	private int customerOrder;
-	
-	@Column(name = "Product ID", nullable = false, length=255)
-	@Size(max=255)
+
+	@Id
+	@ManyToOne
+	@Column(name = "Product ID", nullable = false, length = 255)
+	@Size(max = 255)
 	@NotNull
 	private int productID;
-	
-	@Column(name = "Quantity", nullable = false, length=255)
-	@Size(max=255)
+
+	@Column(name = "Quantity", nullable = false, length = 255)
+	@Size(max = 255)
 	@NotNull
 	private int quantity;
-	
-	@Column(name = "Order Line Prices", nullable = false, length=255)
-	@Size(max=255)
+
+	@Column(name = "Order Line Prices", nullable = false, length = 255)
+	@Size(max = 255)
 	@NotNull
 	private String orderLinePrice;
 
@@ -63,6 +68,5 @@ public class CustomerOrderLine {
 	public void setOrderLinePrice(String orderLinePrice) {
 		this.orderLinePrice = orderLinePrice;
 	}
-	
-	
+
 }
