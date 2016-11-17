ad3c7f195cdf754c436d33d098c084ee51e45dc1
Jake <jstewart7@hotmail.co.uk>
2016-11-17 09:29:23 +0000

    added Login IMS

---
+++ src/main/java/LoginIMS.java
@@ -0,0 +1,20 @@
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class LoginIMS {
+
+	@Id
+	@Column(name = "usernameID", nullable = false, unique = true)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long usernameID;
+	@Column(name = "password", nullable = false, length = 100)
+	@Size(max = 100)
+	@NotNull
+	private String password;
+	
+	
+}
