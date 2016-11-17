a38fc5ecf4a52ccf6a840b1d916891f639cc2117
veram321 <amanvirmehet@yahoo.com>
2016-11-17 14:07:38 +0000

    update product class

--- src/main/java/EntityManagers/ProductManager.java
+++ src/main/java/EntityManagers/ProductManager.java
@@ -1,14 +1,24 @@
 package EntityManagers;
 
+import Entitys.Product;
+
 public interface ProductManager {
 	
-	public Product
+//Create Methods 
+	public Product addProduct(Product Product);
 	
 	
+//Read Methods 
+	public Product displayProductInformation(Product Product);
+	public Product findByID (int ID);
+
+
+//Update Methods 
+	public Product updateProduct(Product Product);
 	
-	
-	
-	
+
+//Delete Methods
+	public Product deleteProduct(Product Product);
 	
 
 }
