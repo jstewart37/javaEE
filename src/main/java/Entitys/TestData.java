cec96cc27b1662c6f446830903e1563b32105889
Sophie <sophiegreenfield31@msn.com>
2016-11-17 16:19:20 +0000

    update push

--- src/main/java/Entitys/TestData.java
+++ src/main/java/Entitys/TestData.java
@@ -12,18 +12,42 @@ public class TestData {
 
 	private List<SupplyOrder> supplyorderlist;
 	private List<Customer> customers;
+	private List<CustomerOrderLine> customerOrderLine;
+	private List<CardDetails> cardDetails;
 
 	@PostConstruct
 	private void setupData() {
 
 		supplyorderlist = new ArrayList<>();
 		customers = new ArrayList<>();
+		customerOrderLine=new ArrayList<>();
+		cardDetails=new ArrayList<>();
 		
 		customers.add(new Customer(1, "mr", "Tom", "Mr.Tom@Movie.com", "12345678912"));
 		customers.add(new Customer(1, "mr", "Tom", "Mr.T*m@M/vie.com", "1912"));
 		
+		customerOrderLine.add(new CustomerOrderLine(4, 2, 1, 2.99 ));
+		cardDetails.add(new CardDetails("21-22-24", "01245879", "MISS G FARTWORTH", "1245789654785412", 2, 2));
+	
+		
 		supplyorderlist.add(new SupplyOrder(1, "Arrived", "2002-02-13 14:37:12", "2008-06-11 11:27:38"));
 		
+	}
+
+	public List<CardDetails> getCardDetails() {
+		return cardDetails;
+	}
+
+	public void setCardDetails(List<CardDetails> cardDetails) {
+		this.cardDetails = cardDetails;
+	}
+
+	public List<CustomerOrderLine> getCustomerOrderLine() {
+		return customerOrderLine;
+	}
+
+	public void setCustomerOrderLine(List<CustomerOrderLine> customerOrderLine) {
+		this.customerOrderLine = customerOrderLine;
 	}
 
 	public List<SupplyOrder> getSupplyorderlist() {
---
+++ src/main/java/OfflineMode/CustomerOrderLineOffline.java
@@ -0,0 +1,37 @@
+package OfflineMode;
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+
+import EntityManagers.CustomerOrderLineManager;
+import Entitys.CardDetails;
+import Entitys.CustomerOrderLine;
+import Entitys.TestData;
+
+@Stateless
+@Default
+
+public class CustomerOrderLineOffline implements CustomerOrderLineManager {
+	@Inject
+	private TestData testData;
+
+	@Override
+	public CustomerOrderLine createNewCustomerOrderLine(CustomerOrderLine customerOrderLine) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CustomerOrderLine updateCustomerOrderLine(CustomerOrderLine customerOrderLine) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CustomerOrderLine removeCustomerOrderLine(CardDetails customerOrderLine) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
