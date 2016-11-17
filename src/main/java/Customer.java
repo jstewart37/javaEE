e15e2f60a4c33839e8adab36d6b19903f04c7bd4
Jake <jstewart7@hotmail.co.uk>
2016-11-17 10:26:04 +0000

    added Login IMS

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -11,26 +11,40 @@ public class Customer {
 	@Column(name = "idCustomer", nullable = false, unique = true)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long idCustomer;
+	
 	@Column(nullable = false, length = 15)
 	@Size(min = 2, max = 225)
 	@NotNull
 	private String title;
+	
 	@Column(nullable = false, length = 100)
 	@Size(min = 3, max = 25)
 	@NotNull
 	private String name;
+	
 	@Column(nullable = false, length = 150)
 	@Size(max = 225)
 	@NotNull
 	@Pattern(regexp = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\." + "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
 			+ "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message = "{invalid.email}")
 	private String email;
+	
 	@Column(nullable = false, length = 20)
 	@Size(max = 225)
 	@NotNull
 	@Pattern(regexp="^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$",
     message="{invalid.phonenumber}")
 	private String phone;	
+	
+	@Column(name = "username", nullable = false)
+	@Size(min = 8)
+	@NotNull
+	private String username;
+	
+	@Column(name = "password", nullable = false)
+	@Size(min = 8)
+	@NotNull
+	private String password;
 	
 	// constructors
 	public Customer(long idCustomer, String title, String name, String email, String phone) {
