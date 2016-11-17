6f6af4fa1f7f1ef38fe655cf1c2b35ce1d94fa54
Sophie <sophiegreenfield31@msn.com>
2016-11-17 11:04:10 +0000

    hjhjhj

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -17,32 +17,6 @@ import javax.validation.constraints.Size;
 public class Address {
 
 	// variables
-
-	@Id
-	@Column(name = "id_Address", nullable = false, unique = true)
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long idAddress;
-	@Column(name = "address_Line_1", nullable = false, length = 100)
-	@Size(max = 100)
-	@NotNull
-	private String addressLine1;
-	@Column(name = "address_Line_2", length = 100)
-	@Size(max = 100)
-	private String addressLine2;
-	@Column(name = "postcode", nullable = false, length = 10)
-	@Size(max = 10)
-	@NotNull
-	private String postCode;
-	@Column(name = "county", nullable = false, length = 100)
-	@Size(max = 100)
-	@NotNull
-	private String county;
-	@Column(name = "city", nullable = false, length = 100)
-	@Size(max = 100)
-	@NotNull
-	private String city;
-
-
 	@Id @Column(name ="id_Address", nullable = false, unique = true) @GeneratedValue(strategy = GenerationType.IDENTITY)private long idAddress;
 	@Column(name ="address_Line_1", nullable = false, length = 100)@Size(max=100) @NotNull private String addressLine1;
 	@Column(name ="address_Line_2", length = 100) @Size(max=100) private String addressLine2;
