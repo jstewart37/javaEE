425d1d26da479339b5c055529be85cbf6053b560
Jake Stewart <tim.spencer95@hotmail.com>
2016-12-06 17:25:05 +0000

    server error fix

--- src/main/java/controllers/AccountDetailsController.java
+++ src/main/java/controllers/AccountDetailsController.java
@@ -4,9 +4,14 @@
 package controllers;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.enterprise.context.*;
 import javax.inject.Named;
+
+import Entitys.*;
+import services.AccountDetailsService;
 
 
 /**
@@ -26,12 +31,35 @@ public String getReturnname(){
 	
 	//String CustomerName = AccountDetailsService.searchBy(email);
 
-	return "Jake Stewart";
+	Customer c = new Customer();
+	
+	String CName = c.getName();
+	return "Jake Stewart"; //CName
 	//return CustomerName;
 	
 }
+
+
+public ArrayList<CustomerOrder> ARCustomerOrders(){
+	ArrayList<CustomerOrder> COList = new ArrayList<CustomerOrder>();
+	COList.add( new CustomerOrder(1, "55", 1, "55", "55", "55", 1)); 
+//	COList.add("World");
+
+	return COList;
+}
+
+public ArrayList<CustomerOrder> orders(){
+	AccountDetailsController ac = new AccountDetailsController();
+	ArrayList<CustomerOrder> COList = ac.ARCustomerOrders();
+	return COList;
 	
+}
+
 public String getemail(){
+	
+	
+	
+	
 	return "jake.stewart@gmail.com";
 }  
 
@@ -53,8 +81,6 @@ public String getcity(){
 public String getpostcode(){
 	return "M6 4PO";
 }
-
-
 
 
 }	
