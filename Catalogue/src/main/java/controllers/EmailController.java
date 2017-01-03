9eec9d4e616555874c50ad92dcd8bdc91480d177
Jake Stewart <tim.spencer95@hotmail.com>
2017-01-03 14:35:47 +0000

    corrected tab page names

---
+++ Catalogue/src/main/java/controllers/EmailController.java
@@ -0,0 +1,76 @@
+package controllers;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.mail.MessagingException;
+import services.MailService;
+
+/**
+ * A bean class that sends an email message
+ * @author Jake Stewart
+ */
+@Named("EmailController")
+@RequestScoped
+public class EmailController implements Serializable {
+
+	@Inject
+	private MailService mailService;
+	
+	
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+    
+    public String send() {
+        statusMessage = "Message Sent";
+        try {
+            MailService.sendMessage(name,email, subject, message);
+        }
+        catch(MessagingException ex) {
+            statusMessage = ex.getMessage();
+        }
+        return "index";  // redisplay page with status message
+    }
+    
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+    public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	private String email;
+	private String subject;
+    private String message;
+    private String statusMessage = "";
+    private String name;
+ 
+} 
